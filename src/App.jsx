9e2c9b10b54a538a4215bb784ec7fc69786b7998
Chandan-kr-dev <chandanramgar@gmail.com>
2024-08-31 21:05:17 +0530

    add login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Login from './components/Login'
+import Register from './components/Register'
+import Hero from './components/Hero'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/Login'} element={<Login/>}></Route>
-      </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Hero/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
