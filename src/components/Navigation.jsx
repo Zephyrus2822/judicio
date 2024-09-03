5dfa3a2ac49cd35a1e99853d177746ea5fa89959
Zephyrus2822 <rudranilchowdhury83@gmail.com>
2024-09-03 13:07:33 +0530

    frontend-ver1.2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,13 +5,12 @@ const Navigation = () => {
         <nav>
           <div className="Logo-wrapper"><img className="Logo" src="./images/judicio-logo.png" alt="logo" /></div>
           <ul>
-            <li className="proj-name" href="#">Judicio</li>
+            <li className="proj-name">Judicio</li>
             <li href="#">Get Verdict</li>
             <li href="#">Verdicts Passed</li>
             <li href="#">About Us</li>
             <li href="#">Contact Us</li>
           </ul>
-          <button className="chat">Help</button>
           <a className="chat" href="/Login">Login</a>
         </nav>
     </div>
