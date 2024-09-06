0a902b63905eb74350a5d39bd8103d1ec4902f71
Chandan-kr-dev <chandanramgar@gmail.com>
2024-09-06 21:32:02 +0530

    Add bail application on same page aur crime details v ho gya

--- src/components/Prisoner.jsx
+++ src/components/Prisoner.jsx
@@ -3,11 +3,13 @@ import "./prisoner.css";
 import axios from "axios";
 
 const Prisoner = () => {
+  const [showapplication, setshowapplication] = useState(false);
+
   const [Name, setName] = useState("");
   const [FatherName, setFatherName] = useState("");
-  const [polstn,setpolstn] =useState("");
-  const [age,setage] =useState("");
-  const [voter,setvoter] = useState("");
+  const [polstn, setpolstn] = useState("");
+  const [age, setage] = useState("");
+  const [voter, setvoter] = useState("");
   const [resadd, setresadd] = useState("");
   const [peradd, setperadd] = useState("");
   const [adharnum, setadharnum] = useState("");
@@ -17,47 +19,46 @@ const Prisoner = () => {
   const [crime, setcrime] = useState("");
   const [gender, setgender] = useState("");
 
-  const handleSubmit =  (e) => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     try {
-       axios.post(`${import.meta.env.VITE_DEV_URL}api/prisonerdets`, {
-        Name,
-        FatherName,
-        polstn,
-        age,
-        resadd,
-        voter,
-        
-        adharnum,
-        prisonbefore,
-        firdate,
-        crime,
-        
-      })
-      .then((response)=>console.log(response))
-      .catch((err)=>console.log(err))
+      axios
+        .post(`${import.meta.env.VITE_DEV_URL}api/prisonerdets`, {
+          Name,
+          FatherName,
+          polstn,
+          age,
+          resadd,
+          voter,
 
-      setName("")
-      setFatherName("")
-      setpolstn("")
-      setage("")
-      setvoter("")
-      setresadd("")
-      setadharnum("")
-      setcrime("")
-      setdatetrial("")
-      setfirdate("")
-      setperadd("")
-      setprisonbefore("")
+          adharnum,
+          prisonbefore,
+          firdate,
+          crime,
+        })
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err));
+
+      // setName("");
+      // setFatherName("");
+      // setpolstn("");
+      // setage("");
+      // setvoter("");
+      // setresadd("");
+      // setadharnum("");
+      // setcrime("");
+      // setdatetrial("");
+      // setfirdate("");
+      // setperadd("");
+      // setprisonbefore("");
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
   return (
-   
     <div className="container">
-      <div className="title">Register Yourself For Bail Application</div>
+      <div className="title">Registration</div>
       <div className="content">
         <form onSubmit={handleSubmit} action="#">
           <div className="user-details">
@@ -186,9 +187,9 @@ const Prisoner = () => {
             <div className="conviction">
               <label
                 htmlFor="crime"
-               
+                style={{ fontSize: "16px;", fontWeight: "500;" }}
               >
-                <span  style={{ fontSize: "16px;", fontWeight: "800;" }}>Convicted For:</span>
+                Convicted For:
               </label>
 
               <input
@@ -248,13 +249,263 @@ const Prisoner = () => {
                   <span className="gender">Other</span>
                 </label>
               </div>
-            <button  className="button" type="submit">Register</button>
+              <button
+                className="button w-10/12 text-center bg-gradient-to-tr from-blue-400 to-red-300 px-2 py-1 rounded-lg flex justify-left text-white font-bold items-center "
+                type="submit"
+              >
+                Register
+              </button>
             </div>
           </div>
         </form>
       </div>
+      <button
+        onClick={() => setshowapplication(!showapplication)}
+        className="text-xl bg-blue-400 px-2 py-1 rounded-lg ml-[45%] mt-5 mb-5 "
+      >
+        Show Application
+      </button>
+      <div className="application w-full min-h-[80%]">
+        {showapplication && (
+          <div className="bailapplication w-[55%] bg-white rounded-lg mx-auto px-20  py-5 ">
+            <div
+              style={{
+                pageBreakBefore: "always",
+                pageBreakAfter: "always",
+                textAlign: "center",
+                marginLeft: "40px",
+                fontFamily: "Roboto",
+                marginTop: "10px",
+              }}
+            >
+              <div>
+                <p style={{ fontWeight: "bold", marginBottom: "15px" }}>
+                  BAIL BOND U/S 437-A CR.P.C. <br />
+                </p>
+                <p style={{ fontWeight: "bold", marginBottom: "15px" }}>
+                  BOND & BAIL BOND FOR ATTENDANCE BEFORE THE APPELLANT COURT{" "}
+                  <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  In the court of Sh.
+                  ____________________________________________________ <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  P.S. :{" "}
+                  <span className="border-b-2 border-black">{polstn}</span>{" "}
+                  <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  U/S ___________________ <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  FIR No. _________________ <br />
+                </p>
+                <p style={{ fontWeight: "bold" }}>
+                  <u>PERSONAL BOND</u> <br />
+                </p>
+                <p style={{ textAlign: "start", padding: "5px auto" }}>
+                  I, <span className="border-b-2 border-black">{Name}</span>{" "}
+                  S/o. Sh.
+                  <span className="border-b-2 border-black">{FatherName}</span>
+                  <br />
+                  R/o
+                  ________________________________________________________________
+                  Having been <br />
+                  acquitted by this Hon&#8217;ble Court on ________________ in
+                  above said case FIR No. <br />
+                  _________________ P.S.{" "}
+                  <span className="border-b-2 border-black">{polstn}</span> U/s
+                  _____________ and required to give <br />
+                  surety for my attendance before the Hon&#8217;ble Court on
+                  condition that I shall attend the Hon&#8217;ble <br />
+                  Appellate Court on every date of hearing in which any appeal
+                  filed against the judgment & <br />
+                  Order of acquittal, passed by this Hon&#8217;ble Court and in
+                  case making default therein I myself <br />
+                  undertake to forfeit to the Govt. of India the sum of Rs.
+                  ____________________ <br />
+                </p>
+                <p style={{ textAlign: "start", padding: "5px auto" }}>
+                  Delhi <br />
+                </p>
+                <p style={{ textAlign: "start", padding: "5px auto" }}>
+                  Date: <br />
+                </p>
+                <p style={{ textAlign: "start", padding: "5px auto" }}>
+                  Signature <br />
+                </p>
+                <p style={{ textAlign: "start", fontWeight: "bold" }}>
+                  <u>SURETY BOND</u> <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  I, <span className="border-b-2 border-black">{Name}</span>{" "}
+                  S/o. Sh.
+                  <span className="border-b-2 border-black">
+                    {FatherName}
+                  </span>{" "}
+                  R/o _______________ <br />
+                  ____________________________________________________ hereby
+                  declare myself for the above said Sh. <br />
+                  <span className="border-b-2 border-black">
+                    {Name}
+                  </span> S/o{" "}
+                  <span className="border-b-2 border-black">{FatherName}</span>{" "}
+                  shall attend the appellate <br />
+                  court every date in which any appeal filed against the
+                  Judgment & Order of acquittal, passed by <br />
+                  this Hon&#8217;ble Court and in case making default therein I
+                  myself undertake to forfeit to the Govt. <br />
+                  of India the sum of Rs. __________________ <br />
+                </p>
+                <p style={{ textAlign: "start" }}>
+                  Dated this ..........................................day of
+                  ....................201 <br />
+                </p>
+                <p>
+                  <br />
+                </p>
+                <p style={{ textAlign: "end" }}>
+                  Signature <br />
+                </p>
+                <p style={{ textAlign: "center", marginRight: "33px" }}>
+                  Presented by:
+                </p>
+              </div>
+
+              <div
+                style={{
+                  pageBreakBefore: "always",
+                  pageBreakAfter: "always",
+                  marginLeft: "40px",
+                  fontFamily: "Roboto",
+                  marginTop: "25px",
+                }}
+              >
+                <div>
+                  <p style={{ textAlign: "center", fontWeight: "bold" }}>
+                    <i>AFFIDAVIT</i>
+                  </p>
+                  <br />
+                  <p>
+                    I
+                    <span className="border-b-2 border-black border-dotted">
+                      {" "}
+                      {Name}{" "}
+                    </span>
+                    son / daughter / wife of
+                    .......................................................................................
+                    <br />
+                    Aged about{" "}
+                    <span className="border-b-2 border-black border-dotted">
+                      {" "}
+                      {age}{" "}
+                    </span>{" "}
+                    R/o
+                    ...............................................................................
+                    <br />
+                    do hereby solemnly affirm and declare as under
+                    ...................................................
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    1. That deponent is the resident of above said address and
+                    having his/her Ration Card no. is
+                    ............................
+                    <br />
+                    .......................... and Election Card No.
+                    <span className="border-b-2 border-black border-dotted">
+                      {voter}
+                    </span>{" "}
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    2. That accused is
+                    ...................................................................................
+                    <br />
+                    of the deponent and deponent has full control over him/her
+                    and capable to produce him/her <br />
+                    before this hon&#8217;ble court.
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    3. That deponent is working as
+                    ........................................ at
+                    ...............................
+                    <br />
+                    T/C. No ......................... earns Rs
+                    ................... per month. <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    4. That deponent is the owner of household articles valued
+                    about of Rs.
+                    .............................................................
+                    <br />
+                    5. That deponent is the owner of the immovable property
+                    bearing No. ............................
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    Measuring .................................. sq. yards
+                    situated at ...........................
+                    <br />
+                    valued not less than Rs
+                    ....................................................................
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    6. That deponent undertakes to produce the accused before
+                    the honourable court on every date <br />
+                    of hearing. <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    7. That I have an F.D.R. No
+                    ................................................... Issued
+                    by
+                    ...........................................................
+                    <br />
+                    For Rs
+                    .......................................................................................
+                    <br />
+                  </p>
+                  <p style={{ marginLeft: "20px" }}>
+                    8. That I own a vehicle No
+                    ....................................................... make
+                    .......................................
+                    <br />
+                    R/C no ..................... at present valued not less than
+                    Rs ....................................
+                    <br />
+                  </p>
+                  <p style={{ fontWeight: "bold", textAlign: "end" }}>
+                    <br />
+                    DEPONENT <br />
+                  </p>
+                  <p style={{ fontWeight: "bold", marginTop: "15px" }}>
+                    VERIFICATION <br />
+                    Verified at Delhi on this ................................
+                    day of 200 ........ that the contents of this Affidavit are
+                    true <br />
+                    and correct to the best of my knowledge & nothing material
+                    has been concealed therefrom, no part of it <br />
+                    is untrue.
+                    <br />
+                  </p>
+                  <p style={{ fontWeight: "bold", textAlign: "end" }}>
+                    DEPONENT <br />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {showapplication && (
+          <button className="text-xl bg-blue-400 px-2 py-1 rounded-lg ml-[45%] mt-5 mb-5 ">
+            Pay the amount and print the application
+          </button>
+        )}
+      </div>
     </div>
-    
   );
 };
 
