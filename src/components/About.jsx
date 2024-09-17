6387776d10a504c101e9576bac3efaf4e7ff6567
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2024-09-18 04:18:58 +0530

    UPDATED the FRONT-END

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import video from '../assets/video03.mp4'
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <div className="video-container">
+        <video autoPlay muted loop className="video-background">
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className='about-container'>
+          <div className='about-info'>
+            <div className='info-1'>
+              <h1>Introduction</h1>
+              <p>This documentaion provides information on how to integrate your application wih our API</p>
+            </div>
+            <div className='info-1'>
+              <h1>Authentication</h1>
+              <p>To access our API , you will need an API key . Please contact our support team for more information</p>
+            </div>
+            <div className='info-1'>
+              <h1>Endpoints</h1>
+              <p>Get/users : Retreives a list of users</p>
+              <p>Post/users : Creates a new User</p>
+              <p>Get/users : Retrieves a user by id</p>
+            </div>
+            <div className='info-1'>
+              <h1>Request Format</h1>
+              <p>   All API requests should be made using the HTTP GET or POST methods. The
+                request body should be in JSON format.
+              </p>
+            </div>
+            <div className='info-1'>
+              <h1>Response Format</h1>
+              <p>    API responses will be in JSON format. The response body will contain a
+                status code and a message indicating the success or failure of the
+                request.
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
-import { AuroraBackground } from './aurora-background';
-import { motion } from 'framer-motion';
+import video from '../assets/video2.mp4'
 
 const Home = () => {
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-start justify-center px-2 w-full"
-      >
-        <div>
-        <div className="logo-text text-3xl md:text-7xl font-bold text-white md:mx-40 md:mr-[400px] text-start" style={{ fontFamily: "Berkshire Swash" }}>
 
-            Welcome to Judicio!
-          </div>
-          <div className="quote-text font-extralight text-3xl md:mx-40 md:text-4xl text-neutral-200 py-4" style={{ fontFamily: "Poppins" }}>
-            Justice delayed is Justice denied. <br />With that thought in mind, we have come up with a Bail reckoning system called Judicio. <br />
-            Uniting technology and the Constitution of India, <br /> to deliver swift and just outcomes for undertrial individuals—<br /> whether it's enduring justice or timely mercy through Bail. 
-          </div>
+
+    <div>
+      <div className="video-container">
+        <video autoPlay muted loop className="video-background">
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="heading">
+          Welcome to Judicio!
         </div>
-
-        <button className="h-16 py-3 px-4 inline-flex items-center gap-2 text-sm font-medium rounded-lg border border-gray-200 text-gray-500 hover:border-blue-600 hover:text-blue-600 focus:outline-none focus:border-blue-600 focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500 dark:hover:border-blue-600 dark:focus:text-blue-500 dark:focus:border-blue-600 md:mx-40" style={{ fontFamily: "Outfit", fontSize: "25px", borderRadius: "20px" }}>
-          <a href="/prisoner">Try Judicio Today</a> 
+        <div className="text-1">
+          Justice delayed is Justice denied
+        </div>
+        <div className='text-2'>
+          With that thought in mind, we have come up with
+        </div>
+        <div className='text-2'>
+          a Bail reckoning system called Judicio.
+        </div>
+        <button class="animated-button">
+          <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+            ></path>
+          </svg>
+          <span class="text"> <a href="/prisoner">Try Judicio</a></span>
+          <span class="circle"></span>
+          <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+            ></path>
+          </svg>
         </button>
-      </motion.div>
-    </AuroraBackground>
+
+      </div>
+
+
+    </div>
   );
 };
 
